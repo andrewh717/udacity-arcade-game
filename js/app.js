aa1a817966b389afd9f08c34ef8e1a3fabf6e831
Andrew Hernandez <andrewh717@gmail.com>
2019-12-31 14:49:52 -0500

    Adding eslint

--- js/app.js
+++ js/app.js
@@ -1,11 +1,12 @@
 // Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
+var Enemy = function(location, speed) {
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+
+    this.x = location.x;
+    this.y = location.y;
+    this.speed = speed;
 };
 
 // Update the enemy's position, required method for game
@@ -14,6 +15,12 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.x += this.speed * dt;
+
+    if (this.x > 505) {
+        this.x = -100;
+        this.speed = 100 + Math.floor(Math.random() * 505);
+    }
 };
 
 // Draw the enemy on the screen, required method for game
